f907bc7bd2f0d0ab05ea7c98b4cd9def9a464a91
lailson.santos <lailson2156@gmail.com>
2020-06-15 21:43:24 -0300

    Adicionando um arquivo pyhton com o csv Dados

--- setup.py
+++ setup.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 10 21:52:28 2020
+Created on Wed May 27 20:53:18 2020
 
 @author: lailson
 """
 
-print("Teste")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+import graphviz
+from sklearn.tree import export_graphviz
+
+credito = pd.read_csv('Dados/Credit.csv')
+previsores = credito.iloc[:,0:20].values
+classe = credito.iloc[:,20].values
+
+labelEncoder = LabelEncoder()
+previsores[:,0] = labelEncoder.fit_transform(previsores[:,0])
+previsores[:,2] = labelEncoder.fit_transform(previsores[:,2])
+previsores[:,3] = labelEncoder.fit_transform(previsores[:,3])
+previsores[:,5] = labelEncoder.fit_transform(previsores[:,5])
+previsores[:,6] = labelEncoder.fit_transform(previsores[:,6])
+previsores[:,8] = labelEncoder.fit_transform(previsores[:,8])
+previsores[:,9] = labelEncoder.fit_transform(previsores[:,9])
+previsores[:,11] = labelEncoder.fit_transform(previsores[:,11])
+previsores[:,13] = labelEncoder.fit_transform(previsores[:,13])
+previsores[:,14] = labelEncoder.fit_transform(previsores[:,14])
+previsores[:,16] = labelEncoder.fit_transform(previsores[:,16])
+previsores[:,18] = labelEncoder.fit_transform(previsores[:,18])
+previsores[:,19] = labelEncoder.fit_transform(previsores[:,19])
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(
+    previsores, classe, test_size = 0.3, random_state = 0)
+
+arvore = DecisionTreeClassifier()
+arvore.fit(x_treino, y_treino)
+
+export_graphviz(arvore, out_file = 'tree.dot')
+previsoes = arvore.predict(x_teste)
+
+confusao = confusion_matrix(y_teste, previsoes)
+taxa_acerto = accuracy_score(y_teste, previsoes)
