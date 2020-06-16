f38a847421d13127af9c50aee684e3735b9fe7c2
lailson.santos <lailson2156@gmail.com>
2020-06-15 22:34:56 -0300

    Adicionado o projeto ao PyCharm

--- setup.py
+++ setup.py
@@ -13,31 +13,31 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
 
 credito = pd.read_csv('Dados/Credit.csv')
-previsores = credito.iloc[:,0:20].values
-classe = credito.iloc[:,20].values
+previsores = credito.iloc[:, 0:20].values
+classe = credito.iloc[:, 20].values
 
 labelEncoder = LabelEncoder()
-previsores[:,0] = labelEncoder.fit_transform(previsores[:,0])
-previsores[:,2] = labelEncoder.fit_transform(previsores[:,2])
-previsores[:,3] = labelEncoder.fit_transform(previsores[:,3])
-previsores[:,5] = labelEncoder.fit_transform(previsores[:,5])
-previsores[:,6] = labelEncoder.fit_transform(previsores[:,6])
-previsores[:,8] = labelEncoder.fit_transform(previsores[:,8])
-previsores[:,9] = labelEncoder.fit_transform(previsores[:,9])
-previsores[:,11] = labelEncoder.fit_transform(previsores[:,11])
-previsores[:,13] = labelEncoder.fit_transform(previsores[:,13])
-previsores[:,14] = labelEncoder.fit_transform(previsores[:,14])
-previsores[:,16] = labelEncoder.fit_transform(previsores[:,16])
-previsores[:,18] = labelEncoder.fit_transform(previsores[:,18])
-previsores[:,19] = labelEncoder.fit_transform(previsores[:,19])
+previsores[:, 0] = labelEncoder.fit_transform(previsores[:, 0])
+previsores[:, 2] = labelEncoder.fit_transform(previsores[:, 2])
+previsores[:, 3] = labelEncoder.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelEncoder.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelEncoder.fit_transform(previsores[:, 6])
+previsores[:, 8] = labelEncoder.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelEncoder.fit_transform(previsores[:, 9])
+previsores[:, 11] = labelEncoder.fit_transform(previsores[:, 11])
+previsores[:, 13] = labelEncoder.fit_transform(previsores[:, 13])
+previsores[:, 14] = labelEncoder.fit_transform(previsores[:, 14])
+previsores[:, 16] = labelEncoder.fit_transform(previsores[:, 16])
+previsores[:, 18] = labelEncoder.fit_transform(previsores[:, 18])
+previsores[:, 19] = labelEncoder.fit_transform(previsores[:, 19])
 
 x_treino, x_teste, y_treino, y_teste = train_test_split(
-    previsores, classe, test_size = 0.3, random_state = 0)
+    previsores, classe, test_size=0.3, random_state=0)
 
 arvore = DecisionTreeClassifier()
 arvore.fit(x_treino, y_treino)
 
-export_graphviz(arvore, out_file = 'tree.dot')
+export_graphviz(arvore, out_file='tree.dot')
 previsoes = arvore.predict(x_teste)
 
 confusao = confusion_matrix(y_teste, previsoes)
